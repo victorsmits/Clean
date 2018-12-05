5b68a5c0278f48c0ddce39a09f482b010481bb5c
victorsmits <victor.smits@student.vinci.be>
2018-12-05 09:31:26 +0100

    up

---
+++ cmake-build-debug/CMakeFiles/Clean.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Clean.dir/main.c.o"
+  "Clean.pdb"
+  "Clean"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Clean.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
